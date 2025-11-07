bded6ccb378fc3db2d288acc033256ead79e4a17
AKKI <2300030796@kluniversity.in>
2025-11-07 11:02:21 +0530

    updated SwaggerConfig file

--- Festivalapi-SpringBoot/src/main/java/com/example/practice/config/SwaggerConfig.java
+++ Festivalapi-SpringBoot/src/main/java/com/example/practice/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.klef.dev.config;
+package com.example.practice.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
@@ -14,8 +14,8 @@ public class SwaggerConfig {
     OpenAPI taskOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Task Management API")
-                        .description("Spring Boot REST API for managing tasks")
+                        .title("Festival API")
+                        .description("Spring Boot REST API for managing festivals")
                         .version("1.0.0")
                         .contact(new Contact()
                                 .name("Developer Team")
